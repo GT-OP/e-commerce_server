bcdac23b9321e870c4f8fc9ad0cb7c0ef461a695
Gautam Tiwari <2020uee0131@iitjammu.ac.in>
2023-08-25 21:58:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cookieParser(""));
-app.use(cors({origin:"http://localhost:3000",credentials:true}));
+app.use(cors({origin:"https://amazon-gt.netlify.app",credentials:true}));
 app.use(router);
 
 
